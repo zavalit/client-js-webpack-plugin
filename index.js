4d3c03d159f3f348ad567dc2fac224ac0330b0cc
Vsevolod Dolgopolov <zavalit@googlemail.com>
2015-09-20 17:03:57 +0200

    introduce logging

--- index.js
+++ index.js
@@ -10,21 +10,29 @@ function ClientJsWebpackPlugin(libraries)
 ClientJsWebpackPlugin.prototype.apply = function(compiler)
 {
   fs.writeFile(this.dest_file_path, '//client.js\n', function(err){
-    console.log(err);
+    if(err !== null)
+      console.log(err);
   })
 
   this.libraries.forEach(function(path){
     fs.readFile(this.vendor_dir_path + path, function(err, data){
-      console.log(err);
+
+      if(err !== null)
+        console.log(err);
+
       this.processData(data);
     }.bind(this));
   }.bind(this))
-
+  
+  console.log(this.dest_file_path + " created!");
 }
 
 ClientJsWebpackPlugin.prototype.processData = function (data){
   fs.appendFile(this.dest_file_path, data, function(err){
-    console.log(err);
+    if(err !== null)
+      console.log(err);
+
+
   })
 }
 
